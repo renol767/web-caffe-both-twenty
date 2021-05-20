ded99122e2cdb801c536b06d2ca150976a49bea8
renol767 <72129061+renol767@users.noreply.github.com>
2021-05-21 06:51:18 +0700

    Update SQL

--- pw2.sql
+++ pw2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 24 Apr 2021 pada 09.37
+-- Waktu pembuatan: 21 Bulan Mei 2021 pada 01.50
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.8
 
@@ -41,6 +41,33 @@ CREATE TABLE `daftarmenu` (
 INSERT INTO `daftarmenu` (`id`, `nama_menu`, `deskripsi`, `harga`) VALUES
 (7, 'minuman', 'ini minuman 2', 0),
 (8, 'makanan', 'enak', 100000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `food`
+--
+
+CREATE TABLE `food` (
+  `id` int(100) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `ingredients` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `rate` varchar(255) NOT NULL,
+  `picturePath` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `food`
+--
+
+INSERT INTO `food` (`id`, `name`, `description`, `ingredients`, `price`, `rate`, `picturePath`) VALUES
+(1, 'Sate Sayur Sultan', 'Sate Sayur Sultan adalah menu sate vegan paling terkenal di Bandung. Sate ini dibuat dari berbagai macam bahan bermutu tinggi. Semua bahan ditanam dengan menggunakan teknologi masa kini sehingga memiliki nutrisi yang kaya.', 'Bawang Merah, Paprika, Bawang Bombay, Timun', '150000', '4.2', 'https://i.pinimg.com/736x/06/7b/28/067b2879e5c9c42ec669bf639c3fbffc.jpg'),
+(2, 'Steak Daging Sapi Korea', 'Daging sapi Korea adalah jenis sapi paling premium di Korea. Namun, untuk menikmatinya Anda tidak perlu jauh-jauh ke Korea Selatan. Steak Sapi Korea Oppa Bandung ini sudah terkenal di seluruh Indonesia dan sudah memiliki lebih dari 2 juta cabang.', 'Daging Sapi Korea, Garam, Lada Hitam', '750000', '4.5', 'https://cdns.klimg.com/dream.co.id/resources/news/2020/04/06/133546/bikin-steak-di-rumah-pastikan-bumbunya-meresap-2004066.jpg'),
+(3, 'Mexican Chopped Salad', 'Salad ala mexico yang kaya akan serat dan vitamin. Seluruh bahan diambil dari Mexico sehingga akan memiliki cita rasa yang original.', 'Jagung, Selada, Tomat Ceri, Keju, Wortel', '105000', '3.9', 'https://i1.wp.com/varminz.com/wp-content/uploads/2019/12/mexican-chopped-salad3.jpg?fit=843%2C843&ssl=1'),
+(4, 'Sup Wortel Pedas', 'Sup wortel pedas yang unik ini cocok banget buat kalian-kalian yang suka pedas namun ingin tetap sehat. Rasanya yang unik akan memanjakan lidah Anda.', 'Wortel, Seledri, Kacang Tanah, Labu, Garam, Gula', '60000', '4.9', 'https://images.immediate.co.uk/production/volatile/sites/2/2016/08/25097.jpg?quality=90&resize=768,574'),
+(5, 'Korean Raw Beef Tartare', 'Daging sapi Korea cincang yang disajikan mentah dan disiram saus spesial dengan toping kuning telur dan taburan biji wijen.', 'Daging Sapi Korea, Telur, Biji Wijen', '350000', '3.4', 'https://cmxpv89733.i.lithium.com/t5/image/serverpage/image-id/478345i84598AB4FEB454CB/image-size/large?v=1.0&px=999');
 
 -- --------------------------------------------------------
 
@@ -118,6 +145,32 @@ CREATE TABLE `push` (
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `id` int(100) NOT NULL,
+  `uid` varchar(255) NOT NULL,
+  `food` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `price` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `transaction`
+--
+
+INSERT INTO `transaction` (`id`, `uid`, `food`, `quantity`, `price`, `total`, `status`) VALUES
+(1, 'WjZ87zduK5Sp842TxHDT6Q70SZY2', 'Korean Raw Beef Tartare', '4', '350000', '1400000', 'Belum Bayar'),
+(2, 'WjZ87zduK5Sp842TxHDT6Q70SZY2', 'Sup Wortel Pedas', '1', '60000', '60000', 'Sudah DiBayar'),
+(3, 'WjZ87zduK5Sp842TxHDT6Q70SZY2', 'Mexican Chopped Salad', '1', '105000', '105000', 'Cancelled'),
+(4, 'test', 'Sate Sayur Sultan', '1', '150000', '150000', 'Belum Bayar');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
@@ -158,7 +211,7 @@ CREATE TABLE `user_information` (
 --
 
 INSERT INTO `user_information` (`uid`, `first_name`, `last_name`, `email`, `address`, `numberphone`, `numberwhatsapp`, `createdAt`) VALUES
-('WjZ87zduK5Sp842TxHDT6Q70SZY2', 'Renol', 'Nindi Kara', 'renol@gmail.com', 'Indramayu', '+62123456789', '+62123456789', '2021-04-24 13:48:50');
+('WjZ87zduK5Sp842TxHDT6Q70SZY2', 'Renol', 'NindiKara', 'renol@mail.com', 'Majalengka', '+62123456789', '+62123456789', '2021-05-06 09:51:28');
 
 --
 -- Indexes for dumped tables
@@ -168,6 +221,12 @@ INSERT INTO `user_information` (`uid`, `first_name`, `last_name`, `email`, `addr
 -- Indeks untuk tabel `daftarmenu`
 --
 ALTER TABLE `daftarmenu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `food`
+--
+ALTER TABLE `food`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -201,6 +260,12 @@ ALTER TABLE `push`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
@@ -221,6 +286,12 @@ ALTER TABLE `user_information`
 --
 ALTER TABLE `daftarmenu`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT untuk tabel `food`
+--
+ALTER TABLE `food`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `keys`
@@ -251,6 +322,12 @@ ALTER TABLE `provinsi`
 --
 ALTER TABLE `push`
   MODIFY `id` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
