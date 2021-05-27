50e4c313af7d04cea9c032af1d9e0f6c578beff2
renol767 <72129061+renol767@users.noreply.github.com>
2021-05-27 12:44:51 +0700

    Promo to News API

--- application/controllers/api/News.php
+++ application/controllers/api/News.php
@@ -5,21 +5,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH . 'libraries/REST_Controller.php';
 require APPPATH . 'libraries/Format.php';
 
-class Promo extends REST_Controller{
+class News extends REST_Controller{
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('Promo_model', 'promo');
+        $this->load->model('News_model', 'news');
     }
 
     public function index_get(){
-        $promo = $this->promo->getPromo();
-        if ($promo) {
-            $this->response($promo, REST_Controller::HTTP_OK);
+        $news = $this->news->getNews();
+        if ($news) {
+            $this->response($news, REST_Controller::HTTP_OK);
         } else{
             $this->response([
                 'status' => false,
-                'message' => 'uid not found'
+                'message' => 'news not found'
             ], REST_Controller::HTTP_NOT_FOUND);
         }
 
--- application/models/Promo_model.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-class Promo_model extends CI_Model{
-    public function getPromo(){
-        return $this->db->get('promo')->result_array();
-    }
-}
