d6f27000690d0a8be5c419e08ffa0d4ae694f2bc
renol767 <72129061+renol767@users.noreply.github.com>
2021-06-02 12:04:55 +0700

    Curl untuk Firebase Cloud Messaging

---
+++ curls/curl.php
@@ -0,0 +1,37 @@
+<?php
+
+$curl = curl_init();
+
+$title = $_POST['judul'];
+$isi = $_POST['isi'];
+$auth_key = "key=AAAApdX1WSE:APA91bFs8mtSzFErbDTKemiqu13oViuPxk_Etb8sMMrx3XcXo5j51mBIzpsaEk95Eih6BTjL1GCPJH8cqNRvxfK7f0SaEyKXNNpZJT9M4VbQVWSm5GmNMwkRG4AbaeQwmyILrEru2VSr";
+$registration_ids = 
+  "deZOnnM3SwCUOfmWQ5fd3m:APA91bHCzXUovDrr7ja9Hyi3ZAD54WFWwtE8_T5LeP-Gzh1oVMdr8D3DHpDxqmMYlr9oOgS4FAhEw233AtCr6T0z1GeZJ4ACDF_r7hBSjCmjJHFZ9iDbPlKKXHkpdcH3-9X1rbryGUCB"
+;
+$fields = array(
+  'to' => $registration_ids,
+  'notification' => array(
+    'title' => $title,
+    'isi' => $isi
+  )
+);
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://fcm.googleapis.com/fcm/send',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => json_encode($fields),
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/json',
+    'Authorization: '. $auth_key
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
---
+++ curls/curl_form.php
@@ -0,0 +1,17 @@
+<form method="post" action="curl.php">
+	<table class="table table-striped">
+		<tr>
+			<td>Judul</td>
+			<td><input type="text" name="judul" value="" required="" class="form-control"></td>
+		</tr>
+		<tr>
+		<tr>
+			<td>Isi</td>
+			<td><input type="text" name="isi" value="" required="" class="form-control"></td>
+
+		<tr>
+			<td>&nbsp;</td>
+			<td><input type="submit" name="submit" value="Simpan" class="btn btn-primary"></td>
+		</tr>
+	</table>
+</form>
